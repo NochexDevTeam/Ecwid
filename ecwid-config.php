a079cd89a8b89cde349829b7bde22fba2e580b4e
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2018-02-23 09:43:11 +0000

    Create ecwid-config.php
    Ecwid Settings

---
+++ ecwid-config.php
@@ -0,0 +1,7 @@
+<?php
+			
+		$x_login =  'Your_API_Login'; // Change 'Your_API_Login' with your API Login ID from the Ecwid Authorize.Net SIM account details page.
+		$hash_value =  'Your Hash Value';  // Change 'Your Hash Value' with your MD5 Hash Value code from the Ecwid Authorize.Net SIM account details page.
+		$x_trans_id = 'Your_Transaction_ID'; // Change 'Your_Transaction_ID' with your Transaction ID from the Ecwid Authorize.Net SIM account details page.
+		$storeURL_ID = "http://app.ecwid.com/authorizenet/Your_Store_ID";	// Change Your_Store_ID with your Store ID from your Ecwid Admin section for example: 0123456.
+?>
